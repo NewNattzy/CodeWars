017cf1e14f6191f427af29a25507be1e71fabf99
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2022-01-27 12:53:55 +0300

    Minor edits

--- Day 1/StopGninnipsMySdrow.cs
+++ Day 1/StopGninnipsMySdrow.cs
@@ -1,13 +1,13 @@
 /*
+    This Task from https://www.codewars.com
     Write a function that takes in a string of one or more words,
     and returns the same string, but with all five or more letter words reversed
     (Just like the name of this Kata). Strings passed in will consist of only letters and spaces.
     Spaces will be included only when more than one word is present.
 */
 
-using System.Collections.Generic;
-using System.Linq;
 using System;
+
 
 public class Kata
 {
@@ -17,28 +17,17 @@ public class Kata
 
         string result = "";
         string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        
         foreach (var word in words)
         {
 
             if (word.Length > 4)
-            {
-
                 for (var i = word.Length - 1; i >= 0; i--)
-                {
-
                     result += word[i];
-
-                }
-              
-            }
+            
             else
-            {
-              
               result += word;
-              
-            }  
-          
-          
+
             result += " ";
 
         }
@@ -46,16 +35,15 @@ public class Kata
         return result.TrimEnd();
 
     }
-
+    
 }
 
 
 /*
-Someone else's decision
-        
-public static string SpinWords(string sentence)
-{
-    return String.Join(" ", sentence.Split(' ').Select(str => str.Length >= 5 ? new string(str.Reverse().ToArray()) : str));
-}
-        
+    Someone else's decision  \/  impressive..
+
+    public static string SpinWords(string sentence)
+    {
+        return String.Join(" ", sentence.Split(' ').Select(str => str.Length >= 5 ? new string(str.Reverse().ToArray()) : str));
+    }    
 */
