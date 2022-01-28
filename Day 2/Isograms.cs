68790865f54488108a9b915f1aa35e38ff9d2410
Nattzy <53081632+NewNattzy@users.noreply.github.com>
2022-01-28 15:03:47 +0300

    Add Task №4
    + Один метод расширения = 1 строка вместо 6.. прикольно

---
+++ Day 2/Isograms.cs
@@ -0,0 +1,45 @@
+/*
+    Isograms -> Task from https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/csharp (chunjef)
+    An isogram is a word that has no repeating letters, consecutive or non-consecutive.
+    Implement a function that determines whether a string that contains only letters is an isogram.
+    Assume the empty string is an isogram. Ignore letter case.
+*/
+
+using System;
+
+
+namespace CodeWars
+{
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CheckIsogram("Dermatoglyphics"));    // True
+            Console.WriteLine(CheckIsogram("isogram"));           // True
+            Console.WriteLine(CheckIsogram("moose"));            // False
+            Console.WriteLine(CheckIsogram("moOse"));           // False
+        }
+
+        private static bool CheckIsogram(string str)
+        {
+            string result = string.Empty;
+
+            foreach (char c in str.ToLower().Distinct())
+                result += c;
+
+            if (str.ToLower() == result)
+                return true;
+
+            return false;
+        }
+
+    }
+
+}
+
+/*
+    Someone else's decision \/
+    return str.ToLower().Distinct().Count()==str.Length;
+*/
